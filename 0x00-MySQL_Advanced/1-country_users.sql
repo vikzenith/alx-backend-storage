7347b4f3f38935acd39c741aaca2478261b939ed
Victor Godwin <124065747+vikzenith@users.noreply.github.com>
2024-03-21 22:45:47 +0100

    commit 2

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,9 @@
--- Write a SQL script that creates a table users
+-- Creates a users table with country enum
 
 CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
